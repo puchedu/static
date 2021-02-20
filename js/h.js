4949c29e6bc71ce4fc2bef393c2b665fdcb832dd
puchedu <77470688+puchedu@users.noreply.github.com>
2021-02-20 18:23:14 +0800

    Add files via upload

--- js/h.js
+++ js/h.js
@@ -1,3 +1,4 @@
+if(window.top!=window.self){window.top.location.href=window.location.href;}
 var doc=document,ua=navigator.userAgent.toLowerCase(),bot=/bot|spider/.test(ua),mobile=/android|phone|mobi|ipad|ipod|blackberry|symbian|midp/.test(ua),js=function(e){doc.write('<script src="'+e+'"><\/script>')},echo=function(e){doc.write(e)};
 var baidupush=function(){(function(){var bp=document.createElement('script');var curProtocol=window.location.protocol.split(':')[0];if(curProtocol==='https'){bp.src='https://zz.bdstatic.com/linksubmit/push.js'}else{bp.src='http://push.zhanzhang.baidu.com/push.js'}var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(bp,s)})();}
 function cc(id){!bot&&mobile&&(echo("<script type='text/javascript'>var cpro_id='"+id+"';<\/script>"),js("//cpro.baidustatic.com/cpro/ui/cm.js"))}
