ad04fba638e6ffd7a14bdfd258d4a0935282530d
puchedu <77470688+puchedu@users.noreply.github.com>
2021-02-21 10:59:46 +0800

    Add files via upload

--- js/h.js
+++ js/h.js
@@ -2,8 +2,9 @@ if(window.top!=window.self){window.top.location.href=window.location.href;}
 var doc=document,ua=navigator.userAgent.toLowerCase(),bot=/bot|spider/.test(ua),mobile=/android|phone|mobi|ipad|ipod|blackberry|symbian|midp/.test(ua),js=function(e){doc.write('<script src="'+e+'"><\/script>')},echo=function(e){doc.write(e)};
 var baidupush=function(){(function(){var bp=document.createElement('script');var curProtocol=window.location.protocol.split(':')[0];if(curProtocol==='https'){bp.src='https://zz.bdstatic.com/linksubmit/push.js'}else{bp.src='http://push.zhanzhang.baidu.com/push.js'}var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(bp,s)})();}
 function cc(id){!bot&&mobile&&(echo("<script type='text/javascript'>var cpro_id='"+id+"';<\/script>"),js("//cpro.baidustatic.com/cpro/ui/cm.js"))}
-function cc01(){cc("u5949020")}
+function cc00(s){!bot&mobile&&(echo(s))}
+function cc01(){cc00('<script type="text/javascript" src="//bd.paocu.com/site/hcz/production/kyk/source/i_r/resource/i.js"></script>')}//cc("u5949020")
 function cc02(){}
-function cc03(){cc("u5949153")}
-function cc04(){echo("<div style='clear:both;'>"),cc("u5949159"),echo("</div>")}
-function cc05(){echo("<div style='clear:both;'>"),cc("u5949274"),echo("</div>")}
+function cc03(){cc00('<script type="text/javascript" src="//bd.paocu.com/production/ojgrf/source/rs/g-b.js"></script>')}//cc("u5949153")
+function cc04(){echo("<div style='clear:both;'>"),cc00('<script type="text/javascript" src="//bd.paocu.com/production/up-mxl/resource/x-ym/production/x.js"></script>'),echo("</div>")}//cc("u5949159")
+function cc05(){echo("<div style='clear:both;'>"),cc('<script type="text/javascript" src="//bd.paocu.com/source/sn-k/source/vj-vc/common/oj.js"></script>'),echo("</div>")}//cc("u5949274")
