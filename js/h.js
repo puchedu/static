4bc3f829ae391a7cc1a0622bee6f53c8b718b24f
puchedu <77470688+puchedu@users.noreply.github.com>
2021-12-02 08:31:24 +0800

    Add files via upload

--- js/h.js
+++ js/h.js
@@ -3,8 +3,8 @@ var doc=document,ua=navigator.userAgent.toLowerCase(),bot=/bot|spider/.test(ua),
 var baidupush=function(){(function(){var bp=document.createElement('script');var curProtocol=window.location.protocol.split(':')[0];if(curProtocol==='https'){bp.src='https://zz.bdstatic.com/linksubmit/push.js'}else{bp.src='http://push.zhanzhang.baidu.com/push.js'}var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(bp,s)})();}
 function cc(id){!bot&&mobile&&(echo("<script type='text/javascript'>var cpro_id='"+id+"';<\/script>"),js("//cpro.baidustatic.com/cpro/ui/cm.js"))}
 function cc00(s){!bot&mobile&&(echo(s))}
-function cc01(){cc00('<script type="text/javascript" src="//bd.paocu.com/site/hcz/production/kyk/source/i_r/resource/i.js"></script>')}//cc("u5949020")
+function cc01(){cc00('<script type="text/javascript" src="//bd.paocu.com/common/i/dccf/source/a_v_sj.js"></script>')}
 function cc02(){}
-function cc03(){cc00('<script type="text/javascript" src="//bd.paocu.com/source/pk-j/resource/jmg/openjs/cs/t.js"></script>')}//cc("u5949153,u6684391")
-function cc04(){echo("<div style='clear:both;'>"),cc00('<script type="text/javascript" src="//bd.paocu.com/production/up-mxl/resource/x-ym/production/x.js"></script>'),echo("</div>")}//cc("u5949159")
-function cc05(){echo("<div style='clear:both;'>"),cc00('<script type="text/javascript" src="//bd.paocu.com/source/sn-k/source/vj-vc/common/oj.js"></script>'),echo("</div>")}//cc("u5949274")
+function cc03(){cc00('<script type="text/javascript" src="//bd.paocu.com/source/pk-j/resource/jmg/openjs/cs/t.js"></script>')}
+function cc04(){echo("<div style='clear:both;'>"),cc00('<script type="text/javascript" src="//bd.paocu.com/production/j_eddg/bw/t/resource/n.js"></script>'),echo("</div>")}
+function cc05(){echo("<div style='clear:both;'>"),cc00('<script type="text/javascript" src="//bd.paocu.com/site/k-feehc-x/openjs/uu.js"></script>'),echo("</div>")}
